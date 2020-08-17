33a158522198a0379c2d881182b4842506bcce7d
Sevindu <wickrama.sevindu06@gmail.com>
2020-08-17 12:13:33 +0530

    Board Us App

---
+++ app/src/main/java/com/example/boardus/OwnerUpdate.java
@@ -0,0 +1,14 @@
+package com.example.boardus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OwnerUpdate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_update);
+    }
+}
---
+++ app/src/main/java/com/example/boardus/Owneregister.java
@@ -0,0 +1,14 @@
+package com.example.boardus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Owneregister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owneregister);
+    }
+}
